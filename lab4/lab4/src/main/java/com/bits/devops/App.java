bc9637ea39ba48e72b5d88c214b65ca1e1a766de
Christy2022wa86820 <2022wa86820@wilp.bits-pilani.ac.in>
2026-09-25 12:47:21 +0530

    Modified App.java to test CI trigger

--- lab4/lab4/src/main/java/com/bits/devops/App.java
+++ lab4/lab4/src/main/java/com/bits/devops/App.java
@@ -2,6 +2,6 @@ package com.bits.devops;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello DevOps CI!");
+        System.out.println("Hello I am christy Jenifer and this is Lab activity 4&5!");
     }
 }
